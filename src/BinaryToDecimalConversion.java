5f037a65590ce6c06714b9d0190755ac1243d8f5
sandeepPangare <pangaresandeep96@gmail.com>
2022-09-06 22:30:59 +0530

    New program

---
+++ src/BinaryToDecimalConversion.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BinaryToDecimalConversion {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a binary number");
+        int num = scanner.nextInt();
+
+        int decimal = 0;
+        int temp = num;
+        int i = 0;
+
+        while (temp > 0) {
+            int r = temp % 10;
+            temp = temp / 10;
+            decimal= decimal + r * (int) Math.pow(2, i++);
+        }
+        System.out.println("Decimal of " + num + " is " + decimal);
+    }
+}
